64e205191c51115b5aabef0093ac1c9bf73af106
Pratyaksha Wirapati <pratyaksha.wirapati@gmail.com>
2020-08-04 16:29:08 +0200

    version 0.0.2; fix version requirement typo

--- R/acplot.R
+++ R/acplot.R
@@ -7,27 +7,32 @@ logit_axt <- c("0.01","0.05","0.1","0.25","0.5","0.75","0.9","0.95","0.99")
 acplot <- function( ac , gene, o=NULL, xf=function(x)log(x+.5),
   col.key = NULL, col.map = NULL,
   pch=20, cex=NULL,bar=FALSE,
-  xt.key=NULL,xt.key2=NULL,
+  xt.key=NULL,
+  xtv=0,xtl=FALSE,
+  xt.key2=NULL,
+  xtv2=0,xtl2=2,
   ...
 )
 {
   gene <- gene[1]
-  nac <- nrow(ac$annot)
+  nac <- nrow(ac$a)
   if(is.null(o)) o <- 1:nac
+  else nac <- length(o)
+
   if(!is.null(col.key) && !is.null(col.map) )
-    col <- col.map[ ac$annot[o,col.key] ]
+    col <- col.map[ ac$a[o,col.key] ]
   else
     col <- rep(1,nac)
   
   if(is.null(cex))
-    cex <- 1 + sqrt(ac$annot$N/max(ac$annot$N))*2
+    cex <- 1 + sqrt(ac$a$N/max(ac$a$N))*2
 
   if(!is.null(xt.key))
-    xt <- rle(ac$annot[o,xt.key])
+    xt <- rle(ac$a[o,xt.key])
   if(!is.null(xt.key2))
-    xt2 <- rle(ac$annot[o,xt.key2])
-
-
+    xt2 <- rle(ac$a[o,xt.key2])
+  
+  par(mar=c(5+xtv+xtv2,4,4,2))
   plot( xf(ac$y[1,o,gene]),
     axes=FALSE, xaxs="i",xlim=c(0,nac+1),
     xlab="",ylab="ave. counts", main=gene, 
@@ -36,15 +41,19 @@ acplot <- function( ac , gene, o=NULL, xf=function(x)log(x+.5),
   axis(side=2,at=xf(as.numeric(log_axt)),labels=log_axt,las=2)
   if(!is.null(xt.key))
     {
-    mtext( xt$values, at=cumsum(xt$lengths)-xt$lengths/2,side=1,line=1.5)
-    #abline(v=0.5+cumsum(xt$lengths),lty=3)
+    mtext( xt$values, at=cumsum(xt$lengths)-xt$lengths/2,side=1,
+      line=1.5,las=ifelse(xtv > 0,2,1))
+    if(xtl) abline(v=0.5+cumsum(xt$lengths),lty=xtl)
     }
   if(!is.null(xt.key2))
     {
-    mtext( xt2$values, at=cumsum(xt2$lengths)-xt2$lengths/2,side=1,line=3)
-    abline(v=0.5+cumsum(xt2$lengths),lty=2)
+    mtext( xt2$values, at=cumsum(xt2$lengths)-xt2$lengths/2,side=1,
+      line=3+xtv,las=ifelse(xtv2 > 0,2,1))
+    if(xtl2) abline(v=0.5+cumsum(xt2$lengths),lty=xtl2)
     }
-  mtext( side=3, at=0,adj=0,text=paste("dataset: ",ac$annot$dataset[1]),line=2)
+  if(!is.null(ac$a$dataset) && length(unique(ac$a$dataset))==1 )
+    mtext( side=3, at=0,adj=0,line=2,
+      text=paste("dataset: ",ac$a$dataset[1]))
   if(bar==TRUE)
   {
     for(i in 1:nac)
--- R/aggr.R
+++ R/aggr.R
@@ -1,33 +1,40 @@
 aggr <- function( x, cellgroups, v0 = 1/12 )
 {
+  require(Matrix)
+
   cellgroups <- as.character(cellgroups)
-  uid <- unique(cellgroups)
-  naggr <- length(uid)
-  ngene <- nrow(x)
-  if( length(cellgroups) != ncol(x) ) stop("length(cellgroups) != ncol(x)")
-  y <- array(0,dim=c(2,naggr,ngene),
-    dimnames=list(c("u","v"),uid,rownames(x)))
+  uid <- na.omit(unique(cellgroups))
+  n_ac <- length(uid)
   a <- data.frame(id=uid,N=0)
   rownames(a) <- a$id
 
-  for(i in uid)
+  if(is.matrix(x) || is(x,"sparseMatrix"))
     {
-    print(i)
-    j <- grep(i,cellgroups,fixed=T)
-    Ni <- length(j)
-    a[i,"N"] <- Ni
+    n_gene <- nrow(x)
+    if(length(cellgroups) != ncol(x))
+      stop("length(cellgroups) != ncol(x)")
+    y <- array(0,dim=c(2,n_ac,n_gene),
+      dimnames=list(c("u","v"),uid,rownames(x)))
 
-    if(Ni == 1 )
+    for(i in uid) # loop over aggregates defined by unique id
       {
-      y[1,i,] <- x[,j]
-      y[2,i,] <- Inf
-      }
-    else
-      {
-      Sx <- Matrix::rowSums( x[,j] )
-      Sxx <- Matrix::rowSums( x[,j]^2 )
-      y[1,i,] <- Sx/Ni
-      y[2,i,] <- (v0 + (Sxx-Sx*Sx/Ni)/(Ni-1))/Ni
+      print(i)
+      j <- grep(i,cellgroups,fixed=T)
+      Ni <- length(j)
+      a[i,"N"] <- Ni
+
+      if(Ni == 1 )
+        {
+        y[1,i,] <- x[,j]
+        y[2,i,] <- Inf
+        }
+      else
+        {
+        Sx <- rowSums( x[,j] )
+        Sxx <- rowSums( x[,j]^2 )
+        y[1,i,] <- Sx/Ni
+        y[2,i,] <- (v0 + (Sxx-Sx*Sx/Ni)/(Ni-1))/Ni
+        }
       }
     }
   list(annot=a,y=y)
